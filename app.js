27beda9a7731ac4869192a3281803b5f60706adb
23angrymonkeys <23angrymonkeys@gmail.com>
2016-02-07 16:16:54 +0300

    trycommit

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var app = angular.module('santa', []);
-app.controller('MailsController', function(){
-	this.mail = "test@ya.ru";
-});
 app.directive('list', function(){
 	return {
-		templateUrl: 'create-list.html'
+		templateUrl: 'create-list.html',
+		controller: ['$scope', function($scope){
+				var mails = [];
+				this.mails = mails;
+				$scope.addNewMail = function(newMail){
+					if(newMail.indexOf("@") != -1)
+					{
+						mails.push(newMail);
+						$scope.newMail = null;
+				    }
+				};
+				$scope.deleteMail = function(idx){
+					mails.splice(idx, 1);
+				};
+		}],
+		controllerAs: 'listMails'
 	};
 });
